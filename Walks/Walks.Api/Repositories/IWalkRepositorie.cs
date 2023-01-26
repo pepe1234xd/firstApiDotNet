729f0cdaa00085935f45fb9cc82c9385458ecc03
pepe1234xd <joseafedox@gmail.com>
2023-01-26 14:31:35 -0600

    finishing the crud operation

--- Walks/Walks.Api/Repositories/IWalkRepositorie.cs
+++ Walks/Walks.Api/Repositories/IWalkRepositorie.cs
@@ -11,5 +11,7 @@ namespace Walks.Api.Repositories
         Task<Walk> AddAsync(Walk walk);
 
         Task<Walk> UpdateAsync(Guid id,Walk walk);
+
+        Task<Walk> DeleteAsync(Guid id);
     }
 }
